c16028474bec36fc14773ed891fd5138bb2f5d11
Hillard28 <56941938+Hillard28@users.noreply.github.com>
2021-07-08 14:38:06 -0400

    Create fred_api.py

---
+++ fred_api.py
@@ -0,0 +1,64 @@
+'''
+Early work in progress. FRED data keys can be found on the website; to find, visit:
+https://fred.stlouisfed.org/
+'''
+# For loading data if you want to skip all the previous inputs
+import requests
+
+class Fred(object):
+    
+    root_url = 'https://api.stlouisfed.org/fred/'
+    
+    def __init__(self, api_key=None):
+        self.api_key = None
+        if api_key is not None:
+            self.api_key = str(api_key)
+        if self.api_key is None:
+            raise ValueError('Please enter an API key')
+        elif len(self.api_key) < 32:
+            raise ValueError('Please enter a valid API key')
+    
+    def get_observations(self,
+                         series_id,
+                         observation_start,
+                         observation_end,
+                         units='',
+                         frequency='',
+                         aggregation_method=''):
+        
+        url = (self.root_url
+               + 'series/observations?series_id=' + series_id
+               + '&observation_start=' + observation_start
+               + '&observation_end=' + observation_end
+               + '&units=' + units
+               + '&frequency=' + frequency
+               + '&aggregation_method=' + aggregation_method
+               + '&api_key=' + self.api_key
+               + '&file_type=json'
+        )
+        
+        request = requests.get(url).json()
+        
+        obs_dates = []
+        obs_values = []
+        
+        for item in request['observations']:
+            obs_dates.append(item['date'])
+            obs_values.append(item['value'])
+        
+        data = {
+            'Date': obs_dates,
+            series_id: obs_values,
+        }
+        
+        return data
+
+
+
+
+
+
+
+
+
+
